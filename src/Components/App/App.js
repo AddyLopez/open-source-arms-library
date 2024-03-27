fba913ab52603fe06dd0570783539bb74d1ce1d9
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-27 00:08:48 -0600

    Initialize map, include Leaflet CSS link

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,34 @@
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
+
+/*import { Panel } from "../Panel/Panel";*/
 import "./App.css";
+
+const { BaseLayer } = LayersControl;
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing</h1>
+      <MapContainer
+        className="Map"
+        center={[40, -40]}
+        zoom={3}
+        scrollWheelZoom={false}
+      >
+        <LayersControl position="topright">
+          <BaseLayer name="OpenStreetMap" checked>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </BaseLayer>
+          <BaseLayer name="Esri World Imagery">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </BaseLayer>
+        </LayersControl>
+      </MapContainer>
     </div>
   );
 }
