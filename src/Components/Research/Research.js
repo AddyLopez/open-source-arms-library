19711068d394f01e2310db392baa42d6251f83b2
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-29 03:29:04 -0600

    Initialize Research component

---
+++ src/Components/Research/Research.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Research.css";
+
+function Research() {
+  return (
+    <>
+      <h2>Hello, from Research!</h2>
+    </>
+  );
+}
+
+export default Research;
