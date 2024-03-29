a9c8532dc030ea26aedb4c3fee0d7de2cd9fa016
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-29 02:13:21 -0600

    Import and package data

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { weaponsArray } from "../../Data/arms-data.js";
 import "./Panel.css";
+
+console.log(weaponsArray);
 
 function Panel() {
   return (
     <main className="Panel">
       <h1>Open Source Arms Library</h1>
+      <div>
+        <p>paragraph</p>
+        <p>paragraph</p>
+      </div>
     </main>
   );
 }
---
+++ src/Data/arms-data.js
@@ -0,0 +1,37 @@
+const dolphinClassSubmarine = require("./weapons/dolphin-class-submarine.json");
+const derbyMissile = require("./weapons/derby-missile.json");
+const dror = require("./weapons/dror.json");
+const eitanApc = require("./weapons/eitan-apc.json");
+const elm2080Greenpine = require("./weapons/elm2080-greenpine.json");
+const jointLightTacticalVehicle = require("./weapons/joint_light_tactical_vehicle.json");
+const m89srSniper = require("./weapons/m89sr-sniper.json");
+const mitznefetCamouflage = require("./weapons/mitznefet-camouflage.json");
+const nimrodAgm = require("./weapons/nimrod-agm.json");
+const python5Missile = require("./weapons/python-5-missile.json");
+const rafaelOws25 = require("./weapons/rafael-ows-25.json");
+const rotax916 = require("./weapons/rotax-916.json");
+const samsonRws = require("./weapons/samson-rws.json");
+const shafrirAirToAirMissile = require("./weapons/shafrir-air-to-air-missile.json");
+const spyder = require("./weapons/spyder.json");
+const tamir = require("./weapons/tamir.json");
+const uziPro = require("./weapons/uzi-pro.json");
+
+export const weaponsArray = [
+  derbyMissile,
+  dolphinClassSubmarine,
+  dror,
+  eitanApc,
+  elm2080Greenpine,
+  jointLightTacticalVehicle,
+  m89srSniper,
+  mitznefetCamouflage,
+  nimrodAgm,
+  python5Missile,
+  rafaelOws25,
+  rotax916,
+  samsonRws,
+  shafrirAirToAirMissile,
+  spyder,
+  tamir,
+  uziPro,
+];
