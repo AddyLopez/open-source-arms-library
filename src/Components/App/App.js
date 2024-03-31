9d58ba871abdefc97f7e10ccb00786a316de2ed9
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-31 02:15:35 -0600

    Plot the markers

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,37 @@ import "./App.css";
 const { BaseLayer } = LayersControl;
 
 function App() {
+  const plotMarkers = () => {
+    const coordinatesList = weaponsArray.map((weaponElement) => {
+      return weaponElement.manufacturing.map((site) => {
+        if (site.coordinates.latitude !== null) {
+          const latitudeFloat = parseFloat(site.coordinates.latitude);
+          const longitudeFloat = parseFloat(site.coordinates.longitude);
+          return [latitudeFloat, longitudeFloat];
+        } else {
+          return `${site.coordinates.latitude}, ${site.coordinates.longitude}`;
+        }
+      });
+    });
+    //console.log(coordinatesList);
+    const filteredCoordinatesList = coordinatesList.filter(
+      (item) => item.includes("null, null") === false
+    );
+    const markers = filteredCoordinatesList.map((item) => {
+      return item.map((element) => {
+        console.log(element);
+        console.log(typeof element);
+        return (
+          <Marker key={element} position={element}>
+            <Popup>{element}</Popup>
+          </Marker>
+        );
+      });
+    });
+    return markers;
+    // console.log(filteredCoordinates);
+  };
+
   return (
     <div className="App">
       <MapContainer
@@ -37,11 +68,7 @@ function App() {
             />
           </BaseLayer>
         </LayersControl>
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {plotMarkers()}
       </MapContainer>
       <Panel data={weaponsArray} />
     </div>
