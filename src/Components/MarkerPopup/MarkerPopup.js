7887f99c2215d4e0d6025702d6558a0eb0fad073
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-04-01 00:03:52 -0600

    Add conditional rendering to marker popups

--- src/Components/MarkerPopup/MarkerPopup.js
+++ src/Components/MarkerPopup/MarkerPopup.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Popup } from "react-leaflet";
+import { weaponsArray } from "../../Data/arms-data.js";
 import "./MarkerPopup.css";
 
-function MarkerPopup({ weaponsIndex }) {
-  console.log(weaponsIndex);
-  return <Popup>Hello, from MarkerPopup!</Popup>;
+console.log(weaponsArray);
+function MarkerPopup({ weaponsIndex, manufacturingIndex }) {
+  console.log(weaponsIndex, manufacturingIndex);
+  let title =
+    weaponsArray[weaponsIndex].manufacturing[manufacturingIndex].site_name;
+  let address =
+    weaponsArray[weaponsIndex].manufacturing[manufacturingIndex].site_address;
+  let coordinates = `${weaponsArray[weaponsIndex].manufacturing[manufacturingIndex].coordinates.latitude}, ${weaponsArray[weaponsIndex].manufacturing[manufacturingIndex].coordinates.longitude}`;
+  console.log(title, address, coordinates);
+  return (
+    <Popup>
+      <h3>{title !== null ? title : "Location's name unspecified."}</h3>
+      <p>{address !== null ? address : "Address unspecified."}</p>
+      <p>{coordinates}</p>
+    </Popup>
+  );
 }
 
 export default MarkerPopup;
