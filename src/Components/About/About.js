42b7d4d82be9ae605b462225f39bf1706dee2b36
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-04-03 01:26:21 -0600

    Set up state for accordion, conditional rendering

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function About() {
+function About({ selected, toggle }) {
+  const aboutHeader = document.getElementById("about-header");
   return (
     <section className="About">
-      <label for="first">About This Application</label>
-      <input type="radio" name="accordian" id="first" checked />
-      <p className="accordian-content">
+      <header id="about-header" onClick={toggle}>
+        <h2>About This Application</h2>
+        <div className="accordion-icon">
+          {selected !== aboutHeader ? "+" : "-"}
+        </div>
+      </header>
+      <p className="accordion-content">
         This application presents data from an open-source library of research
         pertaining to the manufacture of arms and arms' components known to be
         used in U.S. imperialist wars.
