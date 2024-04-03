6317ed7d4c210c5d277e9652da600c662f309bd1
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-04-02 22:45:18 -0600

    Format Research component to integrate accordian

--- src/Components/Research/Research.js
+++ src/Components/Research/Research.js
@@ -2,14 +2,16 @@ import React from "react";
 //import { weaponsArray } from "../../Data/arms-data";
 import "./Research.css";
 
-function Research({ weaponsIndex, manufacturingIndex }) {
+function Research(/*{ weaponsIndex, manufacturingIndex }*/) {
   /*console.log(
     `weaponsIndex: ${weaponsIndex}, manufacturingIndex: ${manufacturingIndex}`
   );*/
   return (
-    <>
-      <h2>Hello, from Research!</h2>
-    </>
+    <section className="Research">
+      <label for="third">About This Application</label>
+      <input type="radio" name="accordian" id="third" />
+      <p className="accordian-content">Research info...</p>
+    </section>
   );
 }
 
