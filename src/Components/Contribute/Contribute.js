ed7024cf904421d5545776cbc9acc70263c09be3
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-05-04 04:38:11 -0600

    Add and style Contribute footer component

---
+++ src/Components/Contribute/Contribute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Contribute.css";
+
+function Contribute() {
+  return (
+    <footer className="Contribute">
+      <h4>To contribute to OSAL: </h4>
+      <a
+        href="/"
+        title="Contribute to this project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        CLICK HERE
+      </a>
+    </footer>
+  );
+}
+
+export default Contribute;
