be795fb28227bd9f73c06654488992c0b724601a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-05-04 19:11:46 -0600

    Add conditional rendering to Contribute footer

--- src/Components/Contribute/Contribute.js
+++ src/Components/Contribute/Contribute.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Contribute.css";
 
-function Contribute() {
+function Contribute({ selected }) {
+  const researchHeader = document.getElementById("research-header");
   return (
-    <footer className="Contribute">
+    <footer
+      className={
+        selected === researchHeader ? "Contribute fixed" : "Contribute static"
+      }
+    >
       <h4>To contribute to OSAL: </h4>
       <a
         href="/"
